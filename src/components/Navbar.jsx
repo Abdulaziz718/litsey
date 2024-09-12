4e1b1f758a7f5afc86047540e07545fbf5799f1c
Abdulaziz718 <githubgaemail@gmail.com>
2024-09-12 16:49:22 +0500

    flexed the responsive for the navbar 9/12/24

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
@@ -8,15 +9,16 @@ const Navbar = () => {
                 <h1 className='text-4xl'>Logo</h1>
             </div>
             <div className='flex gap-5'>
-                <ul className='flex gap-4 text-xl font-normal items-center cursor-pointer'>
+                <ul className='lg:flex gap-4 text-xl hidden font-normal items-center cursor-pointer'>
                     <li>Biz haqimizda</li>
                     <li>Bosh sahifa</li>
                     <li>Bog'lanish</li>
                     <li>Ustozlar</li>
                 </ul>
-                <div className='flex items-start text-lg gap-4'>
+                <div className='flex items-center text-lg gap-4'>
                     <button className='bg-red-600 px-4 py-2 rounded-lg text-white hover transition-all duration-300'>Registratsiya</button>
                     <button className='bg-blue-600 px-4 py-2 rounded-lg text-white hover transition-all duration-300'>Kirish</button>
+                    <button className='text-3xl flex lg:hidden'><GiHamburgerMenu /></button>
                 </div>
             </div>
         </div>
